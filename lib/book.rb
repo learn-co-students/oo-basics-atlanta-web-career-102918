0ae5b918743dac582823892e811113ce5a6e17d1
Andrew Jones <joneseyit@protonmail.com>
2018-10-31 22:06:52 -0400

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -12,30 +12,12 @@ class Book
   # rspec ./spec/01_book_spec.rb:25 # Book properties has a genre
   # rspec ./spec/01_book_spec.rb:32 # Book #turn_page can turn the page
 
-  def title
-    @title
-  end
+
   ##this seems redundant - what are we doing
-  def author=(author)
-    @author = author
-  end
 
-  def author
-    @author
-  end
-  def page_count=(page_count)
-    @page_count = page_count
-  end
 
-  def page_count
-    @page_count
-  end
-  def genre=(genre)
-    @genre = genre
-  end
-  def genre
-    @genre
-  end
+  attr_accessors :author, :page_count, :genre
+  attr_reader :title
 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
